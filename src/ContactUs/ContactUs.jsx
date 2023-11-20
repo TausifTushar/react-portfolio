5a9e6af314485138040cce99de591881c8f9a0e6
unknown <tausifahmed506181@gmail.com>
2023-11-20 09:21:02 +0600

    update contact form

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -1,20 +1,42 @@
+/* eslint-disable react/no-unknown-property */
 import React from 'react';
 
 const ContactUs = () => {
     return (
         <div className='h-[100vh] w-full bg-[#171327] flex justify-center'>
-            <form>
-                <p className='text-white flex justify-center text-3xl font-bold mb-8'>Contact Us</p>
-                <input className='w-[400px] shadow-ccustom border-2 border-fuchsia-600' type="text" name="" id="" />
+            {/* <form>
+                <p classNameName='text-white flex justify-center text-3xl font-bold mb-8'>Contact Us</p>
+                <input classNameName='w-[400px] shadow-ccustom border-2 border-fuchsia-600' type="text" name="" id="" />
                 <br />
-                <input className='w-[400px] shadow-ccustom mt-5 border-2 border-fuchsia-600' type="email" name="" id="" />
+                <input classNameName='w-[400px] shadow-ccustom mt-5 border-2 border-fuchsia-600' type="email" name="" id="" />
                 <br />
-                <input className='w-[400px] shadow-ccustom h-[100px] border-2 mt-5 border-fuchsia-600' type="text" name="" id="" />
+                <input classNameName='w-[400px] shadow-ccustom h-[100px] border-2 mt-5 border-fuchsia-600' type="text" name="" id="" />
                 <br />
-                <div className='flex justify-center mt-5'>
-                <input  type="submit" value="SEND" className='shadow-ccustom shadow-box py-3 px-5 text-2xl rounded-lg text-center bg-fuchsia-600 text-white' />
+                <div classNameName='flex justify-center mt-5'>
+                <input  type="submit" value="SEND" classNameName='shadow-ccustom shadow-box py-3 px-5 text-2xl rounded-lg text-center bg-fuchsia-600 text-white' />
                 </div>
-            </form>
+            </form> */}
+                        <section className="w-[600px] ">
+                        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md ">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-fuchsia-600">Contact Us</h2>
+                        <p className="mb-8 lg:mb-16 font-light text-center text-fuchsia-600 dark:text-fuchsia-600 sm:text-xl"></p>
+                        <form action="#" className="space-y-8">
+                        <div>
+                            <label for="email" className="block mb-2  text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                            <input type="email" id="email" className=" shadow-sm border-2  bg-gray-50 border-fuchsia-600 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-fuchsia-600 block w-full p-2.5 dark:bg-gray-700 dark:border-fuchsia-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-fuchsia-600 dark:shadow-ccustom" placeholder="name@flowbite.com" required />
+                        </div>
+                    <div>
+                        <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-fuchsia-600 shadow-sm focus:ring-primary-500 focus:border-fuchsia-600 dark:bg-gray-700 dark:border-fuchsia-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-ccustom" placeholder="Name" required />
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border-2 border-fuchsia-600 focus:ring-primary-500 focus:border-fuchsia-600 dark:bg-gray-700 dark:border-fuchsia-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-ccustom" placeholder="Leave a comment..."></textarea>
+                    </div>
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ">Send message</button>
+                </form>
+            </div>
+            </section>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,47 @@ export default {
         'custom': '0px 0px 92px 35px rgba(166, 55, 214, 0.9)',
         'ccustom':  '0px 0px 29px 0px rgba(166, 55, 214, 0.9)',
       },
-      
-
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      }
+    
     },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
   },
   plugins: [],
 }
